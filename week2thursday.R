65bb20e2d721646e8a1868efb35b8680d496af3b
Ben Doherty <ben@notionparallax.co.uk>
2013-12-13 15:45:44 +1100

    initial

---
+++ week2thursday.R
@@ -0,0 +1,10 @@
+#week 2 - Thursday
+#KNN supervised clasification
+
+#train on about 70% of the data and test it on the rest
+#if training data is performing well, but breaks on test data: indiacates overfitting.
+
+#data analysis with OS tools - Philipp k janert (overfitting diagram)
+# dtreg.com underfittign and overfitting examples
+
+
