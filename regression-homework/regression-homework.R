e14c165025788240492970ead51216ebe3b7322b
Ben Doherty <ben@notionparallax.co.uk>
2013-12-14 14:25:12 +1100

    make jsonlite friendly format

--- regression-homework/regression-homework.R
+++ regression-homework/regression-homework.R
@@ -1,10 +1,11 @@
-install.packages("jsonlite")
+#if we don't have the package needed, install it
+if("jsonlite" %in% rownames(installed.packages()) == FALSE) {install.packages("jsonlite")}
+
 library(jsonlite)
 
+setwd("/home/ben/Documents/GA-DS/regression-homework")
 
-json_file <- fromJSON("regression-homework/workouts.json")
 
-json_file <- lapply(json_file, function(x) {
-  x[sapply(x, is.null)] <- NA
-  unlist(x)
-})
+#this needs the json to be in the format [{...}] i.e. a list of objects
+# if it is in the more obvious format {'k':[{...}]} then it thinks you have one observation
+json_file <- fromJSON("workouts.json")
