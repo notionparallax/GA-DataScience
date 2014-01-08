0fa8709b2ec8cf7a8a080df044d127c29203e3d3
Ben Doherty <ben@notionparallax.co.uk>
2014-01-08 15:10:24 +1100

    Adding pages

---
+++ iris_knn.r
@@ -0,0 +1,82 @@
+# load required library
+library(class)
+library(ggplot2)
+#library(DMwR) #students can check it out
+
+#################################################
+# PREPROCESSING
+#################################################
+
+data <- iris                # create copy of iris dataframe
+
+summary(data)
+
+pairs(data[1:4],main="Iris Data (red=setosa,green=versicolor,blue=virginica)", pch=21,
+      bg=c("red","green3","blue")[unclass(iris$Species)]) #Base R no packages
+
+plotmatrix(data[1:4]) # using ggplot2 package
+
+ggpairs(data , colour = 'Species' , alpha=0.2) #using the recommended GGally package
+
+labels <- data$Species      # store labels
+data$Species <- NULL        # remove labels from feature set 
+
+#################################################
+# TRAIN/TEST SPLIT
+#################################################
+
+set.seed(1)         # initialize random seed for consistency
+                    # NOTE -- run for various seeds --> need for CV!
+
+train.pct <- 0.7    # pct of data to use for training set
+N <- nrow(data)     # total number of records (150)
+
+train.index <- sample(1:N, train.pct * N)       # random sample of records (training set)
+
+train.index
+train.data <- data[train.index, ]       # perform train/test split
+test.data <- data[-train.index, ]       # note use of neg index...different than Python!
+as.matrix(labels)
+as.matrix(labels)[train.index, ]
+train.labels <- as.factor(as.matrix(labels)[train.index, ])     # extract training set labels
+train.labels
+test.labels <- as.factor(as.matrix(labels)[-train.index, ])     # extract test set labels
+
+#################################################
+# APPLY MODEL
+#################################################
+
+err.rates <- data.frame()       # initialize results object
+
+max.k <- 100
+for (k in 1:max.k)              # perform fit for various values of k
+{
+    knn.fit <- knn(train = train.data,          # training set
+                    test = test.data,           # test set
+                    cl = train.labels,          # true labels
+                    k = k                       # number of NN to poll
+               )
+
+    cat('\n', 'k = ', k, ', train.pct = ', train.pct, '\n', sep='')     # print params
+    print(table(test.labels, knn.fit))          # print confusion matrix
+
+    this.err <- sum(test.labels != knn.fit) / length(test.labels)    # store gzn err
+    err.rates <- rbind(err.rates, this.err)     # append err to total results
+}
+
+#################################################
+# OUTPUT RESULTS
+#################################################
+
+results <- data.frame(1:max.k, err.rates)   # create results summary data frame
+names(results) <- c('k', 'err.rate')        # label columns of results df
+
+# create title for results plot
+title <- paste('knn results (train.pct = ', train.pct, ')', sep='')
+
+# create results plot
+results.plot <- ggplot(results, aes(x=k, y=err.rate)) + geom_point() + geom_line()
+results.plot <- results.plot + ggtitle(title)
+
+# draw results plot (note need for print stmt inside script to draw ggplot)
+print(results.plot)
---
+++ week2thursday.R
@@ -0,0 +1,15 @@
+#week 2 - Thursday
+#KNN supervised clasification
+
+#train on about 70% of the data and test it on the rest
+#if training data is performing well, but breaks on test data: indiacates overfitting.
+
+#data analysis with OS tools - Philipp k janert (overfitting diagram)
+# dtreg.com underfittign and overfitting examples
+
+install.packages(c(class, psych, ggplot2, GGally)) #this needs needs to be c()'d into a vector
+
+library(class)
+library(psych)
+library(ggplot2)
+library(GGally)
