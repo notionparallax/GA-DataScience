c0d4995772b4618963dc31e6863e6ff55c3bcb19
Ben Doherty <ben@notionparallax.co.uk>
2014-06-25 14:07:48 +1000

    I've been very naughty and haven't commited in ages

---
+++ Prediction book analysis/mycfg.py
@@ -0,0 +1,4 @@
+c = get_config()
+c.NbConvertApp.notebooks = ["PB_analysis_FinalWriteup.ipynb"]
+c.NbConvertApp.export_format = 'html'
+c.Exporter.preprocessors = ['extractoutput.ExtractOutputPreprocessor']
