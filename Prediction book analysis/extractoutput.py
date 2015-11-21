8697c3592fb82958e35f8f4bc88841e12345ded1
Ben Doherty <ben@notionparallax.co.uk>
2015-11-21 18:57:38 +1100

    cleaning and fixing rot

--- Prediction book analysis/extractoutput.py
+++ Prediction book analysis/extractoutput.py
@@ -1,49 +1,35 @@
-"""Module containing a preprocessor that extracts all of the outputs from the
+"""A preprocessor that extracts all of the outputs from the
 notebook file.  The extracted outputs are returned in the 'resources' dictionary.
 """
-#-----------------------------------------------------------------------------
-# Copyright (c) 2013, the IPython Development Team.
-#
+
+# Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
-# Imports
-#-----------------------------------------------------------------------------
-
-print "ExtractOutputPreprocessor in use. about to import"
 import base64
 import sys
 import os
 from mimetypes import guess_extension
 
 from IPython.utils.traitlets import Unicode, Set
-#from IPython.utils.traitlets import Set
-from IPython.nbconvert.preprocessors.base import Preprocessor   # IPython / nbconvert / preprocessors
+from .base import Preprocessor
 from IPython.utils import py3compat
-print "ExtractOutputPreprocessor in use. imports done"
-#-----------------------------------------------------------------------------
-# Classes
-#-----------------------------------------------------------------------------
+
 
 class ExtractOutputPreprocessor(Preprocessor):
-    print "ExtractOutputPreprocessor in use."
     """
-    Extracts all of the outputs from the notebook file.  The extracted
+    Extracts all of the outputs from the notebook file.  The extracted 
     outputs are returned in the 'resources' dictionary.
     """
 
     output_filename_template = Unicode(
         "{unique_key}_{cell_index}_{index}{extension}", config=True)
 
-    extract_output_types = Set({'png', 'jpeg', 'svg', 'application/pdf'}, config=True)
+    extract_output_types = Set({'image/png', 'image/jpeg', 'image/svg+xml', 'application/pdf'}, config=True)
 
     def preprocess_cell(self, cell, resources, cell_index):
         """
         Apply a transformation on each cell,
-
+        
         Parameters
         ----------
         cell : NotebookNode cell
@@ -55,26 +41,27 @@ class ExtractOutputPreprocessor(Preprocessor):
             Index of the cell being processed (see base.py)
         """
 
-        #Get the unique key from the resource dict if it exists.  If it does not
+        #Get the unique key from the resource dict if it exists.  If it does not 
         #exist, use 'output' as the default.  Also, get files directory if it
         #has been specified
         unique_key = resources.get('unique_key', 'output')
         output_files_dir = resources.get('output_files_dir', None)
-
+        
         #Make sure outputs key exists
         if not isinstance(resources['outputs'], dict):
             resources['outputs'] = {}
-
+            
         #Loop through all of the outputs in the cell
         for index, out in enumerate(cell.get('outputs', [])):
-
+            if out.output_type not in {'display_data', 'execute_result'}:
+                continue
             #Get the output in data formats that the template needs extracted
-            for out_type in self.extract_output_types:
-                if out_type in out:
-                    data = out[out_type]
+            for mime_type in self.extract_output_types:
+                if mime_type in out.data:
+                    data = out.data[mime_type]
 
                     #Binary files are base64-encoded, SVG is already XML
-                    if out_type in {'png', 'jpeg', 'application/pdf'}:
+                    if mime_type in {'image/png', 'image/jpeg', 'application/pdf'}:
 
                         # data is b64-encoded as text (str, unicode)
                         # decodestring only accepts bytes
@@ -84,29 +71,25 @@ class ExtractOutputPreprocessor(Preprocessor):
                         data = data.replace('\n', '\r\n').encode("UTF-8")
                     else:
                         data = data.encode("UTF-8")
-
-                    # Build an output name
-                    # filthy hack while we have some mimetype output, and some not
-                    if '/' in out_type:
-                        ext = guess_extension(out_type)
-                        if ext is None:
-                            ext = '.' + out_type.rsplit('/')[-1]
-                    else:
-                        ext = '.' + out_type
-
+                    
+                    ext = guess_extension(mime_type)
+                    if ext is None:
+                        ext = '.' + mime_type.rsplit('/')[-1]
+                    
                     filename = self.output_filename_template.format(
                                     unique_key=unique_key,
                                     cell_index=cell_index,
                                     index=index,
                                     extension=ext)
 
-                    #On the cell, make the figure available via
-                    #   cell.outputs[i].svg_filename  ... etc (svg in example)
-                    # Where
-                    #   cell.outputs[i].svg  contains the data
+                    # On the cell, make the figure available via
+                    #   cell.outputs[i].metadata.filenames['mime/type']
+                    # where
+                    #   cell.outputs[i].data['mime/type'] contains the data
                     if output_files_dir is not None:
                         filename = os.path.join(output_files_dir, filename)
-                    out[out_type + '_filename'] = filename
+                    out.metadata.setdefault('filenames', {})
+                    out.metadata['filenames'][mime_type] = filename
 
                     #In the resources, make the figure available via
                     #   resources['outputs']['filename'] = data
--- Prediction book analysis/mycfg.py
+++ Prediction book analysis/mycfg.py
@@ -1,4 +1,4 @@
 c = get_config()
-c.NbConvertApp.notebooks = ["PB_analysis_FinalWriteup.ipynb"]
+c.NbConvertApp.notebooks     = ["PB_analysis_FinalWriteup.ipynb"]
 c.NbConvertApp.export_format = 'html'
-c.Exporter.preprocessors = ['extractoutput.ExtractOutputPreprocessor']
+c.Exporter.preprocessors     = ['extractoutput.ExtractOutputPreprocessor']
